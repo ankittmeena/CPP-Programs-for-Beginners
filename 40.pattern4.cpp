1ac0e81a663afcdd41dc3e00c7d8d7d6d877507c
Ankit Kumar Meena <ankit.k.meena2301@gmail.com>
2023-06-15 13:28:45 +0530

    Add files via upload

---
+++ 40.pattern4.cpp
@@ -0,0 +1,38 @@
+/*
+*
+**
+***
+****
+n=4
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+/*
+1
+22
+333
+4444
+n=4
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
