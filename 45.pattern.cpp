e8140570abe7b78a1fca8edb4459651febf2a38e
Ankit Kumar Meena <ankit.k.meena2301@gmail.com>
2023-06-24 16:55:17 +0530

    Add files via upload

---
+++ 45.pattern.cpp
@@ -0,0 +1,19 @@
+/*
+AAA
+BBB
+CCC
+n=3
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        char ch='A'+i-1;
+        for(int j=1;j<=n;j++){
+            cout<<ch;
+        }
+        cout<<endl;
+    }
+}
